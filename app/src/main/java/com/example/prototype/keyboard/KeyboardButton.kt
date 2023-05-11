a900e22bd322dd052630b5a070ae506eaaa036ce
Keťa <03m.lazarova@gmail.com>
2023-05-11 02:04:53 +0200

    Created visual feedback on button click and turned off ripple effect

--- app/src/main/java/com/example/prototype/keyboard/KeyboardButton.kt
+++ app/src/main/java/com/example/prototype/keyboard/KeyboardButton.kt
@@ -1,22 +1,23 @@
 package com.example.prototype.keyboard
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.prototype.R
+import com.example.prototype.root.bounceClick
 import com.example.prototype.ui.theme.LightBlue
+import com.example.prototype.ui.theme.Typography
 
 
 @Composable
@@ -24,12 +25,15 @@ fun KeyboardButton(
     symbol: String,
     onClick: () -> Unit
 ) {
-    Box(
+    Box(contentAlignment = Alignment.Center,
         modifier = Modifier
             .size(80.dp)
             .clip(CircleShape)
-            .clickable { onClick() },
-        contentAlignment = Alignment.Center
+            .bounceClick()
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) { onClick() }
     ) {
         when (symbol) {
             "Delete" -> Icon(
@@ -40,9 +44,10 @@ fun KeyboardButton(
             )
             else -> Text(
                 text = symbol,
-                textAlign = TextAlign.Center,
-               // style = TextStyle(fontFamily = FontFamily.Monospace)
+                style = Typography.h5
             )
         }
     }
 }
+
+
--- app/src/main/java/com/example/prototype/root/Utils.kt
+++ app/src/main/java/com/example/prototype/root/Utils.kt
@@ -2,18 +2,28 @@ package com.example.prototype.root
 
 import android.content.Context
 import android.content.res.Resources
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.shape.ZeroCornerSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -22,8 +32,17 @@ import com.example.prototype.ui.theme.GreyBackground
 
 
 var screenWidth = 0f
+
 const val horizontalScreenPadding = 20
 const val verticalScreenPadding = 30
+
+fun Modifier.horizontalScreenPadding(): Modifier = padding(
+    horizontal = horizontalScreenPadding.dp
+)
+
+fun Modifier.verticalScreenPadding(): Modifier = padding(
+    vertical = verticalScreenPadding.dp
+)
 
 fun Modifier.screenPadding(): Modifier = padding(
     horizontalScreenPadding.dp,
@@ -45,6 +64,33 @@ fun Modifier.gradient(): Modifier =
         )
     )
 
+fun Modifier.bounceClick() = composed {
+    var buttonPressed by remember { mutableStateOf(false) }
+    val scale by animateFloatAsState(if (buttonPressed) 0.60f else 1f)
+
+    this
+        .graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }
+        .clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            onClick = { }
+        )
+        .pointerInput(buttonPressed) {
+            awaitPointerEventScope {
+                buttonPressed = if (buttonPressed) {
+                    waitForUpOrCancellation()
+                    false
+                } else {
+                    awaitFirstDown(false)
+                    true
+                }
+            }
+        }
+}
+
 @Composable
 fun getScreenWidthDp(): Dp =
     (LocalConfiguration.current.screenWidthDp - 2 * horizontalScreenPadding).dp
@@ -55,8 +101,17 @@ fun getScreenWidthPx(): Float = dpToPx(dp = getScreenWidthDp())
 fun dpToPx(dp: Dp): Float {
     return dp.value * Resources.getSystem().displayMetrics.density
 }
+
 fun vibrate(context: Context, duration: Long = 50) {
     val vibrator = ContextCompat.getSystemService(context, Vibrator::class.java)
-    val effect = VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE)
-    vibrator?.vibrate(effect)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val effect = VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE)
+        vibrator?.vibrate(effect)
+    }
+}
+
+fun clickResponse(context: Context) {
+    vibrate(context)
+    val toneGenerator = ToneGenerator(AudioManager.STREAM_DTMF, 80)
+    toneGenerator.startTone(ToneGenerator.TONE_DTMF_5, 50)
 }
